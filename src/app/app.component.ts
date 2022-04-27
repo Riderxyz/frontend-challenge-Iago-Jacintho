d3bc944e2232806baa1bb0fda5290af46fb94a2d
Iago Favilla <ifavilla@gmail.com>
2022-04-27 11:12:43 -0300

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,28 @@
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
-import { collapseOnLeaveAnimation, expandOnEnterAnimation } from 'angular-animations';
+import {
+  collapseOnLeaveAnimation,
+  expandOnEnterAnimation,
+} from 'angular-animations';
 import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [expandOnEnterAnimation(), collapseOnLeaveAnimation()]
+  animations: [expandOnEnterAnimation(), collapseOnLeaveAnimation()],
 })
 export class AppComponent {
   title = 'frontend-challenge-Iago-Jacintho';
-  isHeaderAvailable = false
+  isHeaderAvailable = false;
   constructor(private routeAdress: Router) {
-    moment.locale('pt-br')
+    moment.locale('pt-br');
     this.routeAdress.events.subscribe((ev) => {
       if (this.routeAdress.url.indexOf('login') !== -1) {
         this.isHeaderAvailable = false;
       } else {
         this.isHeaderAvailable = true;
       }
-
-    })  }
-
+    });
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,32 @@
+import { Router } from '@angular/router';
+import { loginInterface } from './../../models/login.model';
 import { Component, OnInit } from '@angular/core';
+import { zoomInOnEnterAnimation, zoomOutOnLeaveAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations:[zoomInOnEnterAnimation(), zoomOutOnLeaveAnimation()]
 })
 export class HeaderComponent implements OnInit {
+  loginObj: loginInterface = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  goBackBtnAvailable = false;
+  constructor(private routeAdress: Router) {}
 
   ngOnInit(): void {
-  }
+    this.routeAdress.events.subscribe((ev) => {
+      if (this.routeAdress.url.indexOf('home') !== -1) {
+        this.goBackBtnAvailable = false;
+      } else {
+        this.goBackBtnAvailable = true;
+      }
+    });
 
+    this.loginObj = JSON.parse(localStorage.getItem('loginOBJ') as any);
+  }
 }
